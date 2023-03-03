4c08528eedeffd9f06b5f10da94301a53c43c253
Leo Broukhis <leobru@users.noreply.github.com>
2023-03-03 10:01:15 -0800

    Update compile.sh
    Using local scratch disk.

--- compile.sh
+++ compile.sh
@@ -1,9 +1,13 @@
-rm $BESM6_PATH/2222
-touch $BESM6_PATH/2222
+#!/bin/sh
+# Using the scratch disk image in the current directory
+rm -f 2222
+touch 2222
+BESM6_PATH=.:$BESM6_PATH
+export BESM6_PATH
 for mod in disp70 vzu term kachka kit diski mott gnus khlam ; do
-echo -n $mod '	: ' 
-prn $mod.b6 
-grep ОШИБОК= $mod.lst
+    echo -n $mod '	: '
+    ./prn $mod.b6
+    grep ОШИБОК= $mod.lst
 done
 dispak rvs.b6
-verify.pl
+./verify.pl
