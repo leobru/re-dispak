8f41fe3007339faaef02371aa9b205e70f6feb4b
Leo Broukhis <leobru@users.noreply.github.com>
2023-03-03 10:03:38 -0800

    Update verify.pl
    besmtool should be in the path.

--- verify.pl
+++ verify.pl
@@ -6,8 +6,8 @@
 for ($i = 0; $i <= $#start; ++$i) {
     $gold = "G$start[$i]-L$len[$i]";
     $silver = "S$start[$i]-L$len[$i]";
-    system("../besmtool/besmtool dump 2053 --start=$start[$i] --length=$len[$i] --to-file=$gold") unless (-e $gold); 
-    system("../besmtool/besmtool dump 2222 --start=$start[$i] --length=$len[$i] --to-file=$silver");
+    system("besmtool dump 2053 --start=$start[$i] --length=$len[$i] --to-file=$gold") unless (-e $gold); 
+    system("besmtool dump 2222 --start=$start[$i] --length=$len[$i] --to-file=$silver");
     if (system("cmp $gold $silver")) {
         chop ($diff = `cmp -l $gold $silver | wc -l`);
         print "$silver differs in $diff bytes\n";
