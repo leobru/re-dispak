38510d5e227c0b767a46788134ebecdb3e6eba95
Alex Loktionoff <oxy@MB.local>
2023-03-03 17:06:14 +0100

    * fix scripts

--- compile.sh
+++ compile.sh
@@ -1,9 +1,11 @@
+BESM6_PATH=${BESM6_PATH:-build}
+
 rm $BESM6_PATH/2222
 touch $BESM6_PATH/2222
 for mod in disp70 vzu term kachka kit diski mott gnus khlam ; do
-echo -n $mod '	: ' 
-prn $mod.b6 
-grep ОШИБОК= $mod.lst
+	echo -n $mod '	: '
+	./prn $mod.b6
+	grep ОШИБОК= $mod.lst
 done
 dispak rvs.b6
-verify.pl
+./verify.pl
