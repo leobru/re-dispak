0ac280df5950745cf3808af95074483d1f57868f
Leo Broukhis <leobru@users.noreply.github.com>
2023-03-02 22:06:50 -0800

    Create compile.sh
    Compilation script

---
+++ compile.sh
@@ -0,0 +1,9 @@
+rm $BESM6_PATH/2222
+touch $BESM6_PATH/2222
+for mod in disp70 vzu term kachka kit diski mott gnus khlam ; do
+echo -n $mod '	: ' 
+prn $mod.b6 
+grep ОШИБОК= $mod.lst
+done
+dispak rvs.b6
+verify.pl
