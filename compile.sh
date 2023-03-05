d49bfec0d34db026295835d0fa20a272b4291997
Leo Broukhis <leobru@users.noreply.github.com>
2023-03-05 14:37:42 -0800

    Update compile.sh
    Added avmd

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ rm -f 2222
 touch 2222
 BESM6_PATH=.:$BESM6_PATH
 export BESM6_PATH
-for mod in disp70 vzu term kachka kit diski mott gnus khlam ; do
+for mod in disp70 vzu term kachka kit diski mott gnus khlam avmd ; do
     echo -n $mod '	: '
     ./prn $mod.b6
     grep ОШИБОК= $mod.lst
